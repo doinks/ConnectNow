be9d2837196809f10c6870eb186b7e0036f3f378
Austin <41308814+doinks@users.noreply.github.com>
2020-03-12 14:23:52 -0400

    Add files via upload

---
+++ Common/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConnectNow.Common
+{
+    public static class Constants
+    {
+        public static string SqlConnectionString = @"Server=sql.neit.edu,4500;Database=SE265_ConnectNow;User Id=aHines;Password=neit2020;";
+    }
+}
